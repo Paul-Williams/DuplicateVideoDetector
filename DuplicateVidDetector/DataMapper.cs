f5267ab23f2706d89465571091a900e9b317ec7b
Paul Williams <p.b.williams@outlook.com>
2023-04-08 00:04:41 +0100

    Recreated file lost when TFS crapped out.

---
+++ DuplicateVidDetector/DataMapper.cs
@@ -0,0 +1,11 @@
+﻿using LiteDB;
+using PW.IO.FileSystemObjects;
+
+namespace DuplicateVidDetector;
+internal class DataMapper : BsonMapper
+{
+  public DataMapper()
+  {
+    RegisterType(filePath => filePath.ToString(), bsonValue => new FilePath(bsonValue));
+  }
+}
